b59f85e0e9408377e53028b05b42a7bc7e67909d
Anderson Jimenez <andersonmurdog@gmail.com>
2020-02-19 11:32:24 -0500

    Primer componetente weatherLocation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// importamos nuestro componente
+import WeatherLocation from './components/WeatherLocation'
 
 function App() {
   return (
@@ -19,6 +21,8 @@ function App() {
           Learn React
         </a>
       </header>
+      {/* llamamos nuestro componente  */}
+      <WeatherLocation/>
     </div>
   );
 }
---
+++ src/components/WeatherLocation.js
@@ -0,0 +1,8 @@
+import React from 'react'; // Siempre se importa reac
+
+const WeatherLocation = () => (
+    <div> <h2>Weather Location</h2> </div>
+);
+
+// Siempre se exporta el componente
+export default WeatherLocation;
