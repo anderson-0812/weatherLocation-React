c2109cc3288bb67942c768088f31be2f35d682b6
Anderson Jimenez <andersonmurdog@gmail.com>
2020-02-21 11:02:42 -0500

    Refactorizacion de nombres y ubicaciones, se toma en cuenta aqui ue en importaciones cuando se importa un directorio poor defecto buscará el infex.js

--- src/components/WeatherLocation/WeatherData/WeatherTempeture.js
+++ src/components/WeatherLocation/WeatherData/WeatherTempeture.js
@@ -10,7 +10,7 @@ import {
     RAIN,
     SNOW,
     WINDY,
-} from './../constants/weathers'
+} from './../../../constants/weathers'
 
 const icons = {
     [CLOUD]:"cloud",
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,6 +1,8 @@
 import React from 'react'; // Siempre se importa reac
 //  Importamos oitro componente que creamos
 import Location from './Location'
+// en una importacion solo conelnombre de carpeta automaticamente busca el idex.js 
+// es lo mismo que tener => './WeatherData/index.js'
 import WeatherData from './WeatherData'
 
 const WeatherLocation = () => (
